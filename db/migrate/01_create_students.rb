fa299defeae98321970c06db766ea14cee3dfcef
Roberto Livi <robertomlivi@outlook.com>
2020-03-25 20:42:47 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,5 +1,5 @@
-class CreateStudents < ActiveRecord::Migration[5.2]
-  # def change
-  #   create_table :
-  # end
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :
+  end
 end
